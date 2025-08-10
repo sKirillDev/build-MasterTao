95856372b0b77a7315b81986bc23fd2bf78d2411
sKirillDev <k.sychov.dp@gmail.com>
2025-08-10 23:48:30 +0300

    created sections advantages, delivery and services

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,32 +6,32 @@ searchboxRemove.forEach(el => el.remove())
 
 
 const btnBurger = document.querySelector('.hamburger')
-btnBurger.addEventListener('click', ()=> btnBurger.classList.toggle('is-active'))
+btnBurger.addEventListener('click', () => btnBurger.classList.toggle('is-active'))
 
 const swiperBanner = new Swiper('.swiper-banner', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  spaceBetween: 24,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 24,
 
-  // If we need pagination
-  pagination: {
-    clickable: true,
-    el: '.swiper-pagination',
-  },
+    // If we need pagination
+    pagination: {
+        clickable: true,
+        el: '.swiper-pagination',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
 });
 
 const header = document.querySelector('header');
 const banner = document.querySelector('.swiper-banner');
 const headerHeight = header.offsetHeight;
 
-banner.setAttribute('style', '--header-height:'+ headerHeight + 'px' );
+banner.setAttribute('style', '--header-height:' + headerHeight + 'px');
 console.log(header.offsetHeight);
 
 const fileInput = document.querySelectorAll('[type="file"]');
@@ -42,3 +42,70 @@ fileInput.forEach(item => {
         fileName.innerHTML = item.files[0].name;
     })
 })
+
+
+const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length
+
+const swiperAdvantages = new Swiper('#advantages', {
+    spaceBetween: 12,
+    loop: true,
+    simulateTouch: true,
+    slidesPerView: 1,
+    roundLengths: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        enabled: true,
+    },
+    breakpoints: {
+        992: {
+            spaceBetween: 0,
+            slidesPerView: 'auto',
+            slidesPerGroup: advantagesSlideLength,
+            loop: false,
+            simulateTouch: false,
+            pagination: {
+                enabled: false,
+                el: '.swiper-pagination',
+            }
+        }
+    }
+});
+
+document.querySelectorAll('.template-carousel').forEach((carouselEl, index) => {
+    const swiper = new Swiper(carouselEl.querySelector('.swiper'), {
+        slidesPerView: 1,
+        direction: 'horizontal',
+        loop: true,
+        spaceBetween: 30,
+
+        navigation: {
+            nextEl: carouselEl.querySelector('.swiper-button-next'),
+            prevEl: carouselEl.querySelector('.swiper-button-prev'),
+        },
+
+        pagination: {
+            el: carouselEl.querySelector('.swiper-pagination'),
+            clickable: true,
+        },
+
+        breakpoints: {
+            830: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+                pagination: {
+                    enabled: false,
+                    el: carouselEl.querySelector('.swiper-pagination'),
+                }
+            },
+            1400: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                pagination: {
+                    enabled: false,
+                    el: carouselEl.querySelector('.swiper-pagination'),
+                }
+            }
+        }
+    });
+});
